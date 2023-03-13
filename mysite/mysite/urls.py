9fd36646720a448750617cb702dc004234685d48
alex <kr@list.com>
2023-03-13 14:24:06 +0300

    урок 5, зачет с замечанием

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shopapp/', include('shopapp.urls')),
+    path('shopapp/', include('shopapp.urls')),  # TODO без app будет лучше, просто shop/
     path('req/', include('requestdataapp.urls')),
 ]
 
