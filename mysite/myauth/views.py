531ef80fda3e6f678e439e75d7c5835c017f25c3
Станислав Стукалов <fols.djenkins@gmail.com>
2023-04-08 12:16:35 +0300

    Запушил

--- mysite/myauth/views.py
+++ mysite/myauth/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpRequest
+from django.contrib.auth.views import LogoutView
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.urls  import reverse, reverse_lazy
 # Create your views here.
 
 
@@ -16,3 +18,33 @@ def login_view(request: HttpRequest):
         login(request, user)
         return redirect("/admin/")
     return render(request, "myauth/login.html", {"error": "Invalid login credentials"})
+
+
+def logout_view(request: HttpRequest):
+    logout(request)
+    return redirect(reverse("myauth:login"))
+
+
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy("myauth:login")
+
+def set_cookie_view(request: HttpRequest) -> HttpResponse:
+    response = HttpResponse("Cookie set")
+    response.set_cookie("fizz", "buzz", max_age=3600)
+    return response
+
+
+def get_cookie_view(request: HttpRequest) -> HttpResponse:
+    value = request.COOKIES.get("fizz", "default  value")
+    return HttpResponse(f"Cookie value: {value!r}")
+
+
+def set_session_view(request: HttpRequest) -> HttpResponse:
+    request.sessin["foobar"] = "spameggs"
+    return HttpResponse("Session set!")
+
+
+def get_session_view(request: HttpRequest) -> HttpResponse:
+    value = request.session.get("foobar", "default")
+    return HttpResponse(f"Session value: {value!r}")
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite.myauth.apps.MyauthConfig',
+    'myauth.apps.MyauthConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/admin/'
