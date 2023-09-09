a93310441edaf98e35c667e26e6715fd01c5d21a
Станислав Стукалов <fols.djenkins@gmail.com>
2023-09-09 20:04:30 +0300

    Сделал

--- Docs/mysite/shopapp/views.py
+++ Docs/mysite/shopapp/views.py
@@ -1,3 +1,8 @@
+"""
+В этом модуле лежат различные наборы представлений.
+
+Разные view интернет-магазина по товарам, заказам и т.д.
+"""
 from timeit import default_timer
 
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, JsonResponse
@@ -9,13 +14,17 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-
+from drf_spectacular.utils import extend_schema, OpenApiResponse
 from .forms import ProductForm
 from .models import Product, Order, ProductImage
 from .serializers import ProductSerializer
 
-
+@extend_schema(description="Product view CRUD")
 class ProductViewSet(ModelViewSet):
+    """
+    Набор представлений для действий над Product
+    Полный CRUD для сущностей товара
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [
@@ -36,6 +45,18 @@ class ProductViewSet(ModelViewSet):
         "price",
         "discount",
     ]
+
+    @extend_schema(
+        summary="Get one product by ID",
+        description="Retrieves product, return 404 if not  found",
+        responses={
+            200: ProductSerializer,
+            404: OpenApiResponse(description="Empty response, product dy id not found"),
+        }
+        )
+    def retrieve(self, *args, **kwargs):
+        return super().retrieve(*args, **kwargs)
+
 
 
 class ShopIndexView(View):
